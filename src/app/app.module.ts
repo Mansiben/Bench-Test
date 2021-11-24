7c313d65b4b857bd3477cf7963983de6f92917e0
Mansiben Buddhdev <mansithakkar74@gmail.com>
2021-11-24 18:07:13 -0500

    Created template with angular application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MatTableModule,MatTableDataSource} from "@angular/material/table";
+import {MatPaginatorModule} from "@angular/material/paginator"
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatTableDataSource,
+    MatPaginatorModule,
     AppRoutingModule
   ],
   providers: [],
