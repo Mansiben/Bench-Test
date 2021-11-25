ae1945995f7566526c069afe4c6156e7a3059d8a
Mansiben Buddhdev <mansithakkar74@gmail.com>
2021-11-25 16:06:46 -0500

    In progress: Add sum of amount in the header.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,10 +13,12 @@ import { map } from 'rxjs/operators';
 export class AppComponent {
   title = 'restTest1';
 
-  displayedColumns = ['date', 'ledger', 'amount', 'company'];
+  displayedColumns = ['date','company','account', 'amount',];
  
   dataSource:any;
   pageEvent: any ;
+  SumData : any =[];
+  sum : any= 0;
 
   constructor(private dataService: DataService) { }
 
@@ -24,25 +26,47 @@ export class AppComponent {
 
 
     ngOnInit(): void {
-      this.initDataSource();
+     this.initDataSource();
+      
     }
 
     initDataSource() {
       this.dataService.findAll(1).pipe(
-        map((userData: TransactionData) => this.dataSource = userData)
-      ).subscribe();
+        map((userData: TransactionData) => this.dataSource = userData,
+        this.SumData.push(this.dataSource) )
+      ).subscribe(res => {
+        this.SumData.push(res);
+        console.log(this.SumData[1].transactions);
+        this.SumData[1].transactions.forEach( (data: any) => {
+          this.sum += parseFloat(data.Amount);
+        });
+        console.log(this.sum);
+      });
+     
+      
+
+     
+      
     }
 
     onPaginateChange(event: PageEvent) {
       let page = event.pageIndex;
       let size = event.pageSize;
+      console.log(this.sum);
   
   
   
         page = page +1;
         this.dataService.findAll(page).pipe(
           map((userData: TransactionData) => this.dataSource = userData)
-        ).subscribe();
+        ).subscribe(res => {
+          this.SumData.push(res);
+          console.log(this.SumData[1].transactions);
+          this.SumData[1].transactions.forEach( (data: any) => {
+            this.sum += parseFloat(data.Amount);
+          });
+          console.log(this.sum);
+        });
       
   
     }
